9b40a31726f755d5254f4e5fb25695c074fff77d
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-29 22:46:42 +0500

    add server

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, {Express} from 'express'
 import {userRouter} from "./users/users";
+import {Server} from "http";
 
 export class App {
     private app: Express;
+    private server: Server
     private port: number;
 
     constructor(app: Express = express(), port: number = 8000) {
@@ -14,6 +16,7 @@ export class App {
         this.app.use('/users', userRouter)
     }
     public async init() {
+        this.useRoutes();
 
     }
 }
