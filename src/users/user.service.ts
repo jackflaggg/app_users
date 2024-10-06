6a8299ed823f353efa9e1c8c5c7b44b6b269fe25
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-06 20:57:17 +0500

    add loggerService

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -2,18 +2,20 @@ import {UserServiceInterface} from "./user.service.interface";
 import {UserRegisterDto} from "./dto/user-register.dto";
 import {User} from "./user.entity";
 import {UserLoginDto} from "./dto/user-login.dto";
+import {injectable} from "inversify";
 
+injectable()
 export class UserService implements UserServiceInterface {
-    createUser(dto: UserRegisterDto): User | null{
+    async createUser(dto: UserRegisterDto): Promise<User | null>{
         const user = new User(dto.email, dto.name);
-
+        await user.setPassword(dto.password);
         if (!user){
             return null;
         }
 
         return user;
     };
-    validateUser(dto: UserLoginDto): boolean {
+    async validateUser(dto: UserLoginDto): Promise<boolean> {
         return true;
     };
 }
