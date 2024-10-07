b5f07349f22f49b4f3182a69ae833b75980c5eeb
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-07 20:07:44 +0500

    add loggerService

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,10 +1,13 @@
 import {UserRegisterDto} from "./dto/user-register.dto";
 import {User} from "./user.entity";
 import {UserLoginDto} from "./dto/user-login.dto";
-import {injectable} from "inversify";
+import {inject, injectable} from "inversify";
+import {TYPES} from "../types";
+import {IConfigService} from "../config/config.service.interface";
 
 @injectable()
 export class UserService implements UserService {
+    constructor(@inject(TYPES.ConfigService) private configService: IConfigService) {}
     async createUser(dto: UserRegisterDto): Promise<User | null>{
         const user = new User(dto.email, dto.name);
         await user.setPassword(dto.password);
