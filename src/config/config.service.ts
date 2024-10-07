adbf037912fdf17e168a196e6f2beb6cf21632bf
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-07 19:58:06 +0500

    add loggerService

---
+++ src/config/config.service.ts
@@ -0,0 +1,26 @@
+import {IConfigService} from "./config.service.interface";
+import { config, DotenvConfigOutput, DotenvParseOutput } from 'dotenv'
+import {inject, injectable} from "inversify";
+import {TYPES} from "../types";
+import {ILoggerService} from "../logger/logger.interface";
+
+@injectable()
+export class ConfigService implements IConfigService {
+    private config: DotenvParseOutput | undefined;
+    constructor(
+        @inject(TYPES.ILoggerService) private logger: ILoggerService,
+    ){
+        const result: DotenvConfigOutput = config();
+        if (result.error){
+            this.logger.error('Не удалось прочитать .env файл');
+        } else {
+            this.config = result.parsed as DotenvParseOutput;
+        }
+    }
+    get<T extends number | string>(key: string): T {
+        if (!this.config) {
+            throw new Error('Конфигурация не инициализирована!');
+        }
+        return this.config[key] as T;
+    }
+}
