fe883885460c1899a8873bf3740faaa9b258ee80
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-09 22:53:07 +0500

    add loggerService

--- src/common/validate.middleware.ts
+++ src/common/validate.middleware.ts
@@ -1,13 +1,15 @@
 import {IMiddleware} from "./middleware.interface";
 import {NextFunction, Request, Response} from "express";
-import {UserService} from "../users/user.service";
 import {ClassConstructor, plainToClass} from "class-transformer";
 import {validate} from "class-validator";
 
 export class ValidateMiddleware implements IMiddleware {
     constructor(private classToValidate: ClassConstructor<object>) {}
+
     execute(req: Request, res: Response, next: NextFunction): void {
+
         const instance = plainToClass(this.classToValidate, req.body);
+
         validate(instance).then((err) => {
             if (err.length) {
                 res.status(422).send({errorsMessages: err})
